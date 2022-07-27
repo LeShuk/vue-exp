d22632a5e758c37cb757df706c41198956bd4c5f
leshuk <89050170077@mail.ru>
2022-07-27 17:03:52 +0400

    version 0.1.1 Двустороннее связывание

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
-    Hello, World!
-    <div>
-      И тебе привет!
-      <strong> {{likes}} </strong>
-      <button @click="like">+</button>
-    </div>
-    <div>Забодал...
-      <strong> {{dislikes}} </strong>
-      <button @click="dislike">-</button>
+  <div class="app">
+<!-- Отключаем для формы действия браузера по умолчанию-->
+    <form @submit.prevent>
+<!--   Вариант двухстороннего связывания через функцию-->
+      <input
+          v-bind:value="formTitle"
+          @input="inputTitle"
+          class="input"
+          type="text"
+          placeholder="Название"/>
+<!--  Вариант двухстороннего связывания через сокращенную запись-->
+      <input
+          v-bind:value="formBody"
+          @input="formBody = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Описание"/>
+      <button class="btn" @click="createPost">Разместить пост</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong>{{post.title}}</div>
+      <div><strong>Описание</strong> {{post.body}}</div>
     </div>
   </div>
 </template>
@@ -18,21 +30,72 @@ export default {
   name: "App",
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        {id: 1, title: 'Дневничок разработчика (нет)', body: 'Изучаю vue.js, полет нормальный...'},
+        {id: 2, title: 'Дневничок разработчика (нет)', body: 'Цикличный вывод данных в компоненте.'},
+        {id: 3, title: 'Мысли вслух',
+          body: 'Немного подбешивает, что приходится тексты выдумывать. Знаю, есть генераторы, но искать их зело лень.'},
+      ],
+      formTitle: '',
+      formBody: '',
+      idCounter: 4,
     }
   },
   methods: {
-    like() {
-      this.likes += 1;
+    createPost() {
+      const newPost = {
+        id: this.idCounter,
+        title: this.formTitle,
+        body: this.formBody,
+      }
+      this.posts.push(newPost);
+      this.formTitle = '';
+      this.formBody = '';
+      this.id += 1;
     },
-    dislike() {
-      this.dislikes += 1;
+    inputTitle(event) {
+      this.formTitle = event.target.value;
     },
   }
 }
 </script>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.app {
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  align-self: flex-end;
+
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 10px;
+}
+.input {
+  width: 100%;
+  padding: 10px 15px;
+  margin-top: 15px;
+  border: 1px solid teal;
+
+}
 </style>
