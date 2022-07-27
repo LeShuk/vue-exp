60deee7a6738d3ecdd2ab48d0074521f327cc46f
leshuk <89050170077@mail.ru>
2022-07-27 15:39:44 +0400

    version 0.1.0 Базовые понятия и простейший пример

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Hello, World!
+    <div>
+      И тебе привет!
+      <strong> {{likes}} </strong>
+      <button @click="like">+</button>
+    </div>
+    <div>Забодал...
+      <strong> {{dislikes}} </strong>
+      <button @click="dislike">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    like() {
+      this.likes += 1;
+    },
+    dislike() {
+      this.dislikes += 1;
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
